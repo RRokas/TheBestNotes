3260ea6e23104e4229482b6ac81d15616e750fc2
RRokas <rokas.ringaila@gmail.com>
2022-07-06 15:51:37 +0300

    Create and get note endpoints

--- TheBestNotes/Controllers/Notes.cs
+++ TheBestNotes/Controllers/Notes.cs
@@ -1,20 +1,45 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.JsonWebTokens;
 using TheBestNotes.Models;
+using TheBestNotes.Models.DTO;
+using TheBestNotes.Services;
 
 namespace TheBestNotes.Controllers;
 
 public class Notes : Controller
 {
-    private readonly SqliteDb _dbContext;
+    private readonly INoteService _noteService;
 
-    public Notes(SqliteDb dbContext)
+    public Notes(INoteService noteService)
     {
-        _dbContext = dbContext;
+        _noteService = noteService;
     }
 
+    [Authorize]
     [HttpPost("Create")]
-    public void CreateNote([FromBody] string title, [FromBody] string content)
+    public void Create([FromBody] BaseNoteDTO noteData)
     {
-        
+        var owner = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+        var ownerGuid = new Guid(owner);
+        _noteService.AddNewNote(ownerGuid, noteData.Title, noteData.Content);
+    }
+
+    [Authorize]
+    [HttpGet("GetSingle")]
+    public IActionResult GetSingle([FromQuery] Guid noteId)
+    {
+        var requester = GetRequesterGuid();
+        var hasAccessToNote = _noteService.HasAccessToNote(requester, noteId);
+
+        if (hasAccessToNote)
+            return Ok(_noteService.GetNote(requester, noteId));
+        return Unauthorized();
+    }
+
+    private Guid GetRequesterGuid()
+    {
+        return new Guid(User.FindFirst(ClaimTypes.NameIdentifier).Value);
     }
 }
